af7d9f156cdd6481abe4d6daaf247169fe98e216
mithunumesan <mithun.umesan@ucdconnect.ie>
2022-02-14 11:47:00 +0000

    updates the weather api link

---
+++ weather_api.py
@@ -0,0 +1,17 @@
+# to get the instantaneous weather data from the api 
+# latitude and longitude have been given as a location in dublin
+# can be changed later on when required to do so
+import time
+import requests
+import json
+import traceback
+weather_api = 'https://api.openweathermap.org/data/2.5/onecall?lat=53.3065282883422&lon=-6.225434257607019&exclude={part}&appid='
+with open('weather_key.txt') as f:
+    weather_key = ''.join(f.readlines())
+while True:
+    try:
+        weather = json.loads(requests.get(weather_api + weather_key).text)
+        print(weather)
+        time.sleep(5*60) #will stop the program for 5 minutes before trying again
+    except:
+        print(traceback.format_exc())
