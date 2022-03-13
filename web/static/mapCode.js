d720f2d4291ede7b6cc05d02acdbc510bc024561
Michael Davitt <63263218+michaeldavitt@users.noreply.github.com>
2022-03-13 16:11:36 +0000

    Created a function to update the station popup with realtime availability info

--- web/static/mapCode.js
+++ web/static/mapCode.js
@@ -20,18 +20,23 @@ function initMap() {
                 const marker = new google.maps.Marker({
                 position:  {lat: station_info[i].position_lat, lng: station_info[i].position_lng},
                 map : map,
-                title : station_info[i].address,
+                title : station_info[i].number.toString(),
                 });
 
 
 
                 // Creates a pop-up window for each station
                 const infowindow = new google.maps.InfoWindow({
-                    content: '<div id="content"><h4>' + station_info[i].address + '<h4></div>',
+                    content: '<div id="station_popup_' +
+                    station_info[i].number +
+                    '" "class="station_popup"><h4>' + 
+                    station_info[i].address + 
+                    '</h4></div>',
                 });
 
                 // Add function that displays pop-up window when a marker is clicked for each station marker
                 marker.addListener("click", () => {
+                    updateInfoWindow(marker.title);
                     infowindow.open({
                         anchor: marker,
                         map,
@@ -55,4 +60,27 @@ function initMap() {
     .always(function(){
         console.log("complete")
     })         
+}
+
+// Function which sends station information to the info window popup
+function updateInfoWindow(station_id){
+    var jqxhr = $.getJSON("/availability/" + station_id, function(data){
+        console.log("success", data)
+        var availabilityData = data;
+
+        infoWindowDiv = document.getElementById("station_popup_" + station_id);
+        availabilityHeader = document.createElement("h4");
+        availability = document.createTextNode("Available Bikes: " + availabilityData[0]["available_bikes"]);
+        availabilityHeader.appendChild(availability);
+        infoWindowDiv.appendChild(availabilityHeader);
+    })
+    .done(function(){
+        console.log("second success");
+    })
+    .fail(function(){
+        console.log("error");
+    })
+    .always(function(){
+        console.log("complete");
+    })
 }
